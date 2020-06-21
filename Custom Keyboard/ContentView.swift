519cb4785095ba53af1abbc0217f0bbc9965755d
CANU1832 <canu1832@gmail.com>
2020-06-22 06:48:26 +0900

    Initial Commit

---
+++ Custom Keyboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Keyboard
+//
+//  Created by 전효재 on 2020/06/22.
+//  Copyright © 2020 전효재. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom KeyboardTests/Custom_KeyboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  Custom_KeyboardTests.swift
+//  Custom KeyboardTests
+//
+//  Created by 전효재 on 2020/06/22.
+//  Copyright © 2020 전효재. All rights reserved.
+//
+
+import XCTest
+@testable import Custom_Keyboard
+
+class Custom_KeyboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
